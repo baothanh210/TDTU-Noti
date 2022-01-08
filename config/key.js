27671f4b9971b96ba7fbd2222fb92119b314ad5a
baothanh210 <dlbt0210@gmail.com>
2022-01-08 15:45:03 +0700

    upload key.js

--- config/key.js
+++ config/key.js
@@ -1,4 +1,4 @@
 module.exports = {
     googleClientID: '627546188309-8e7ien7hohugbs8p3jmbegmsm6ci794b.apps.googleusercontent.com',
-    googleClientSecret: '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'
+    googleClientSecret: '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'
   };
