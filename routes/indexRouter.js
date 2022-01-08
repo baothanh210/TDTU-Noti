9f306290ae4bd31dbf4a417ae1fbd408cb218b6d
baothanh210 <dlbt0210@gmail.com>
2022-01-08 20:21:28 +0700

    upload

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -139,9 +139,10 @@ router.get('/auth/google/callback',
     function (req,res) {
         // Successful authentication, redirect success
         // Check if email is OK or not
-
-        if (domainCheck(studentProfile.hd.value) == 1)
+        console.log(studentProfile)
+        if (domainCheck(studentProfile.hd) == 1)
         {
+
             // Check in database if the student is there or not with googleId
             Student.find({googleId: studentProfile.id}, (err, data) => {
                 if (err) {
