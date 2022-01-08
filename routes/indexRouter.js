8ad8979a676d548e7ae4346f44433359d6a82fe6
IrisH <51488916+tye2002@users.noreply.github.com>
2022-01-08 18:20:16 +0700

    Update indexRouter.js

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -129,7 +129,7 @@ passport.use( new GoogleStrategy ({ clientID: keys.googleClientID,
 ));
 
 router.get('/auth/google',
-    passport.authenticate('google', {scope: ['https://www.googleapis.com/auth/plus.login']}), 
+    // passport.authenticate('google', {scope: ['https://www.googleapis.com/auth/plus.login']}), 
     passport.authenticate('google', {scope : ['https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/userinfo.email']}),
     console.log(studentProfile)
 );
