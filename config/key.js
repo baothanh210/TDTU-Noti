4598dacc291e184499b3c816713db08b956dbe43
IrisH <51488916+tye2002@users.noreply.github.com>
2022-01-08 15:58:42 +0700

    where .env :))))

--- config/key.js
+++ config/key.js
@@ -2,3 +2,4 @@ module.exports = {
     googleClientID: '627546188309-8e7ien7hohugbs8p3jmbegmsm6ci794b.apps.googleusercontent.com',
     googleClientSecret: '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'
   };
+
